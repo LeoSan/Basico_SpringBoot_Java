75fde185de25cb1f258a0024fe205be6142e271f
Leonard Cuenca <cuenca623@gmail.com>
2021-02-07 01:45:47 -0600

    PrimeraPuerta

---
+++ src/main/java/com/platzi/marken/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.platzi.marken;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Saludar")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Nunca pares de aprender mamaton ";
+    }
+}

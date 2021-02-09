0ed03586b276146282d746d38fa1298ad7cadb77
Leonard Cuenca <cuenca623@gmail.com>
2021-02-08 23:59:36 -0600

    Autor: Leonard Cuenca Descripción: Como conectar con una base de datos.

--- src/main/java/com/platzi/marken/web/controller/HolaMundoController.java
+++ src/main/java/com/platzi/marken/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.platzi.marken;
+package com.platzi.marken.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

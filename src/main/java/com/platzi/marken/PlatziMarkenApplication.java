045f16d38782bc859748754079ae05f80cd9607f
Leonard Cuenca <cuenca623@gmail.com>
2021-02-16 17:15:55 -0600

    Autor: Leonard Cuenca Descripción: Implementar la anotación @Service Paso 6

---
+++ src/main/java/com/platzi/marken/PlatziMarkenApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.marken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarkenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarkenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/marken/PlatziMarkenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.marken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarkenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
